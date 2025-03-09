59587631a3881e4a09a77a7b110dcd9003727de6
Victor Hernandez <victormanu968@gmail.com>
2025-03-09 16:34:38 -0500

    IMPL: synchronous communication between orders and inventory with OpenFeign

---
+++ order-service/src/main/java/com/vho/microservices/order/client/InventoryClient.java
@@ -0,0 +1,14 @@
+package com.vho.microservices.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+	boolean isInStock(@RequestParam(name = "skuCode") String skuCode, @RequestParam(name = "quantity") Integer quantity);
+
+}
--- order-service/src/main/java/com/vho/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/vho/microservices/order/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.vho.microservices.order.client.InventoryClient;
 import com.vho.microservices.order.dto.OrderRequest;
 import com.vho.microservices.order.model.Order;
 import com.vho.microservices.order.repository.OrderRepository;
@@ -15,14 +16,27 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 	
 	private final OrderRepository orderRepository;
+	private final InventoryClient inventoryClient;
+	
+	
 	
 	public void placeOrder(OrderRequest orderRequest) {
-		Order order = new Order();
-		order.setOrderNumber(UUID.randomUUID().toString());
-		order.setPrice(orderRequest.price());
-		order.setSkuCode(orderRequest.skuCode());
-		order.setQuantity(orderRequest.quantity());
 		
-		orderRepository.save(order);
+		// hago uso de la funcion definida en la interface, openfeign se encarga de la implementacion
+		var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+		
+		//verifico que el producto este disponible
+		if (isProductInStock) {
+			Order order = new Order();
+			order.setOrderNumber(UUID.randomUUID().toString());
+			order.setPrice(orderRequest.price());
+			order.setSkuCode(orderRequest.skuCode());
+			order.setQuantity(orderRequest.quantity());
+			
+			orderRepository.save(order);
+		}else {
+			throw new RuntimeException("Producto con codigo " + orderRequest.skuCode() + " no esta disponible.");
+		}
+
 	}
 }
