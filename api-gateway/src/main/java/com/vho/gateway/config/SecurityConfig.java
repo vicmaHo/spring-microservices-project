f006c312c27c3f89da26c3eab30df791eac275e7
Victor Hernandez <victormanu968@gmail.com>
2025-03-14 22:41:49 -0500

    FIX: error in propierties for keycloak config fixed

--- api-gateway/src/main/java/com/vho/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/vho/gateway/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.vho.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
