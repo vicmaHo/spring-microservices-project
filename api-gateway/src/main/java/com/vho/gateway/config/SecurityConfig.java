0331c008402a7174ba6f3bc94865c62ccc3b538c
Victor Hernandez <victormanu968@gmail.com>
2025-03-29 23:18:02 -0500

    ADD: freeurls for doc added to the filterchain security

--- api-gateway/src/main/java/com/vho/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/vho/gateway/config/SecurityConfig.java
@@ -8,10 +8,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	
+	private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
+			"/api-docs/**", "/aggregate/**"};
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+		return httpSecurity.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(freeResourceUrls).permitAll() // permito todas las peticiones a las urls definidas
+				.anyRequest()
 				.authenticated())
 				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
 				.build();
