5a7984d2006b2682ae3a7fff9396df6207597f24
Victor Hernandez <victormanu968@gmail.com>
2025-04-07 23:56:09 -0500

    CONFIG (order-service): added timeout config to the clientconfig

--- order-service/src/main/java/com/vho/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/vho/microservices/order/config/RestClientConfig.java
@@ -1,8 +1,13 @@
 package com.vho.microservices.order.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -19,9 +24,19 @@ public class RestClientConfig {
 	public InventoryClient inventoryClient() {
 		RestClient client = RestClient.builder()
 				.baseUrl(inventoryServiceUrl)
+				.requestFactory(getClientRequestFactory())
 				.build();
 		var restClientAdapter = RestClientAdapter.create(client);
 		var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 		return httpServiceProxyFactory.createClient(InventoryClient.class);
+	}
+	
+	
+	private ClientHttpRequestFactory getClientRequestFactory() {
+		ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+				.withConnectTimeout(Duration.ofSeconds(3))
+				.withReadTimeout(Duration.ofSeconds(3));
+		
+		return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings)
 	}
 }
